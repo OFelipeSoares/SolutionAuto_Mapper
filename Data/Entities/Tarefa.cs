dde29c8a26e6b23ee78e0cd59fd5b4c6b56de561
Felipe Soares <118938422+OFelipeSoares@users.noreply.github.com>
2023-06-25 17:12:32 -0300

    25/06 - Projeto Crud Com C# - Lista de Tarefas

---
+++ Data/Entities/Tarefa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("Tarefa")]
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+
+        [NotMapped]
+
+        public List<ItemTarefa> ItensTarefa { get; set; }
+    }
+}
